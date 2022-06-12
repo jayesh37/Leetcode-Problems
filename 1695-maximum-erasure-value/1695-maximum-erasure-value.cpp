17ec44bc20e0c9789322ae87053c3a840ad4f83b
Jayesh Kusuma <44980194+jayesh37@users.noreply.github.com>
2022-06-13 00:47:35 +0530

    Time: 640 ms (29.53%), Space: 127.2 MB (24.42%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /*
+    
+    Move right pointer if we find that element already present
+    
+    
+    */
+    int maximumUniqueSubarray(vector<int>& nums) {
+        unordered_map<int,int> m; // last occuring index of element 
+        int l=0,sum=0,ans=0,n=nums.size();
+        
+        for(int r=0;r<n;r++)
+        {
+            int x = nums[r];
+            
+            if(m.find(x)!=m.end())  //if already present
+            {
+                int index = m[x];
+                while(l<=index)
+                {
+                    m.erase(nums[l]);
+                    sum -= nums[l];
+                    l++;
+                }
+            }
+            m[x]=r;
+            sum += x;
+            ans = max(ans,sum);
+        }
+        
+        return ans;
+    }
+};
