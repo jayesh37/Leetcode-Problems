cd7c20d6d0d2ea8bffc0f71716a048cbf14cfeae
Jayesh Kusuma <44980194+jayesh37@users.noreply.github.com>
2022-06-14 16:01:30 +0530

    Time: 1020 ms (5.46%), Space: 375.9 MB (5.02%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int dp[501][501];
+    
+    int getlcs(string x,string y,int n,int m)
+    {
+        if(n==0||m==0)
+            return dp[n][m]=0;
+        
+        if(dp[n][m]!=-1)
+            return dp[n][m];
+        
+        if(x[n-1]==y[m-1])
+            dp[n][m] = 1 + getlcs(x,y,n-1,m-1);
+        
+        else
+            dp[n][m] = max(getlcs(x,y,n-1,m) , getlcs(x,y,n,m-1));
+        
+        return dp[n][m];
+    }
+    
+    int minDistance(string word1, string word2) {
+        memset(dp,-1,sizeof(dp));
+        
+        int n = word1.size();
+        int m = word2.size();
+        int ans = getlcs(word1,word2,n,m);
+        
+        return n+m-2*ans;
+    }
+};
